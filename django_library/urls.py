cf2a8e1ed37a75a0319d4a730c203791b28ca4a7
AveWycc220 <mik16748@gmail.com>
2020-03-16 01:53:47 +0300

    Create Hello, World

--- django_library/urls.py
+++ django_library/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns = [
+    url(r'^riddles/', include('riddles.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
