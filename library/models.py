617880a246b6e93c9d7eb68100dfaf31fc1a22e6
AveWycc220 <mik16748@gmail.com>
2020-04-17 18:42:22 +0300

    Added Input_Book model

--- library/models.py
+++ library/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.shortcuts import reverse
+class Input_Book(models.Model):
+    title = models.CharField(db_index=True, max_length=150)
+    author = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=150, unique=True)
+    body = models.TextField(blank=True, db_index=True)
+    date_pub = models.DateTimeField(auto_now_add=True)
 
-# Create your models here.
+    def get_absolute_url(self):
+        return reverse("post_detail_url", kwargs={"slug": self.slug})
+
+    def __str__(self):
+        return f'{self.title}'
+        
